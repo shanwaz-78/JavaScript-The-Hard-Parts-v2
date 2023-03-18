ad564fe63b36c5c38aedd09b3092cdeb50dcc412
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-03-18 18:07:44 +0530

    Learned Call Stack

---
+++ Js/JavaScript Principles/call stack.js
@@ -0,0 +1,11 @@
+// Call Stack:-
+
+// When we exicute a function at that time whatever fucntions are made that are stored in call stack;
+
+// JavaScript keeps track of what function is currently running[where's the thread of execution];
+
+// Run a function-add to call stack
+
+//Finish running the function-JS removes it form call stack.
+ 
+// Whatever is top of the call stack:- that's the function we're currently running.
