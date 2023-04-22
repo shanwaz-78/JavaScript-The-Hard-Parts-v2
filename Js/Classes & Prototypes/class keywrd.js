ea59cd149960547ceb882ce5cfb13093caa573a1
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-04-22 09:17:57 +0530

    Completed Course

--- Js/Classes & Prototypes/class keywrd.js
+++ Js/Classes & Prototypes/class keywrd.js
@@ -5,6 +5,10 @@ class scStudent {
     this.name = name;
     this.age = age;
   }
+  info(){
+    const data = 'Hello';
+    console.log(data);
+  }
 }
 const obj = new scStudent("John", 19);
 console.log(obj);
@@ -12,3 +16,4 @@ console.log(obj);
 class secondStudent extends scStudent {}
 const user = new secondStudent("Sentence", 20);
 console.log(user);
+    
